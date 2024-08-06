c84c9b592eea8c6133bc0c5cd4a50252757e203b
Xinhao <panxinhao98@gmail.com>
2024-08-06 17:26:24 +1000

    保存本地更改

--- luto/tools/create_task_runs/bash_scripts/slurm_cmd.sh
+++ luto/tools/create_task_runs/bash_scripts/slurm_cmd.sh
@@ -12,15 +12,31 @@ fi
 
 export GRB_LICENSE_FILE=$grb_license
 
-
 # Create a temporary script file
 SCRIPT=$(mktemp)
 
-# Write the script content to the file
-cat << OUTER_EOF > $SCRIPT
+# Write the script content to the file, directly substituting variables
+cat << 'OUTER_EOF' > $SCRIPT
 #!/bin/bash
-# Activate the Conda environment
-source ~/miniforge3/etc/profile.d/conda.sh
+
+# Read the settings_bash file ==> NODE, TIME, MEM, THREADS, JOB_NAME
+source luto/settings_bash.py
+if [[ $NODE == "dgx" ]]; then
+    echo "Current node is dgx, syncing files from hpc-fc-b-1:/run/user/219976/python to hpc-dgx-b-1:/run/user/219976/python."
+    
+    ssh s222552331@hpc-fc-b-1 "rsync -avz --delete /run/user/219976/python/ s222552331@hpc-dgx-b-1:/run/user/219976/python/"
+    
+    if [ $? -eq 0 ]; then
+        echo "Files synced successfully."
+    else
+        echo "Error occurred during file sync."
+        exit 1
+    fi
+else
+    echo "Current node is not dgx, no sync performed."
+fi
+
+source /run/user/219976/python/miniforge3/etc/profile.d/conda.sh
 conda activate luto
 
 # Run the simulation
@@ -31,13 +47,18 @@ sim.run(data=data, base=2010, target=2050)
 from luto.tools.write import write_outputs
 write_outputs(data)
 INNER_EOF
+
+if [ $? -eq 0 ]; then
+    /home/s222552331/rsync/sync_files.sh
+fi
+
 OUTER_EOF
 
 # Submit the job
 sbatch -p ${NODE} \
     --time=${TIME} \
     --mem=${MEM} \
-    --cpus-per-task=${CPU_PER_TASK} \
+    --cpus-per-task=${THREADS} \
     --job-name=${JOB_NAME} \
     ${SCRIPT}
 
