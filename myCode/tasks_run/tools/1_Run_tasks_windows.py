6539fd8f4255d740acd76ec88cddb547cbf835d4
Xinhao <panxinhao98@gmail.com>
2024-11-21 11:37:15 +1100

    create env

--- myCode/tasks_run/tools/1_Run_tasks_windows.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    os.chdir('../..')  # 这里改变工作目录
-    from myCode.tasks_run_in_windows.tools.helpers import create_task_runs
-    create_task_runs(use_multithreading=True, num_workers=3)
-
-if __name__ == "__main__":
-    main()
