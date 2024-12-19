16c185a6ed1750e1df7a94becd39094b8ad709b5
Xinhao <panxinhao98@gmail.com>
2024-12-19 12:25:46 +1100

    update run years

--- myCode/tasks_run/1_Run_tasks_windows2.py
+++ myCode/tasks_run/1_Run_tasks_windows2.py
@@ -2,8 +2,8 @@ from myCode.tasks_run.tools.helpers import create_task_runs
 import os
 
 if os.name == 'nt':
-    input_file = 'Custom_runs/setting_template_windows_0_failed.csv'
-    create_task_runs(input_file, use_multithreading=True, num_workers=5)
+    input_file = 'Custom_runs/setting_template_windows_01.csv'
+    create_task_runs(input_file, use_multithreading=False, num_workers=5)
 else:
     print("This script is designed to run only on Windows.")
 
