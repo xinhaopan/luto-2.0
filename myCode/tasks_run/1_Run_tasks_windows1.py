e7c65ca1266141a94be0ed360e97523e9b5d1e23
Xinhao <panxinhao98@gmail.com>
2024-12-19 11:23:24 +1100

    update draw code

--- myCode/tasks_run/1_Run_tasks_windows1.py
+++ myCode/tasks_run/1_Run_tasks_windows1.py
@@ -3,6 +3,6 @@ import os
 
 if os.name == 'nt':
     input_file = 'Custom_runs/setting_template_windows_0_test.csv'
-    create_task_runs(input_file, use_multithreading=True, num_workers=6)
+    create_task_runs(input_file, use_multithreading=True, num_workers=3)
 else:
     print("This script is designed to run only on Windows.")
