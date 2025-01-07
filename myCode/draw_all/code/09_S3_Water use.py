1ff56fae4f817aa8e0b406640b08a953cdebc56a
Xinhao <panxinhao98@gmail.com>
2025-01-07 21:37:59 +1100

    make tables

--- myCode/draw_all/code/09_S3_Water use.py
+++ myCode/draw_all/code/09_S3_Water use.py
@@ -51,9 +51,16 @@ import settings
 csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Value (ML)', 'Landuse Type'
 water_dict = get_dict_from_json(input_files, 'water_1_water_net_use_by_broader_category')
 water_dict,legend_colors = get_colors(water_dict, 'tools/land use colors.xlsx', sheet_name='lu')
+
+water_limit_dict = get_dict_data(input_files, 'water_yield_limits_and_public_land', 'Value (ML)', 'Type')
+water_yeild_dict ={key: df.assign(**{'Yield': df.sum(axis=1)}) for key, df in water_dict.items()}
+water_point_dict = concatenate_dicts_by_year([water_limit_dict, water_yeild_dict])
+water_point_dict = rename_and_filter_columns(water_point_dict, ['WNY LIMIT','Yield'], ['Constarints','Yield'])
+point_colors = ['red','black']
+
 output_png = '../output/09_S3_water.png'
 y_range, y_ticks = calculate_y_axis_range(water_dict)
-plot_Combination_figures(water_dict, output_png, input_files, plot_stacked_bar, legend_colors,
+plot_Combination_figures(water_dict, output_png, input_files, plot_stacked_bar_and_line, legend_colors,point_dict=water_point_dict,point_colors=point_colors,
                             n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=y_range,
                              x_ticks=20, y_ticks=y_ticks,
                              legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=2)
