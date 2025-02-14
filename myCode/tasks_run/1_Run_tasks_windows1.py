8f80b32fbff2e9182b913b31be251314ee984401
Xinhao <panxinhao98@gmail.com>
2025-02-14 14:19:56 +1100

    change to time-series by year

--- myCode/tasks_run/1_Run_tasks_windows1.py
+++ myCode/tasks_run/1_Run_tasks_windows1.py
@@ -2,7 +2,7 @@ from myCode.tasks_run.tools.helpers import create_task_runs
 import os
 
 if os.name == 'nt':
-    input_file = 'Custom_runs/setting_template_windows_0131_1.csv'
+    input_file = 'Custom_runs/setting_template_windows_0214_1.csv'
     create_task_runs(input_file, use_multithreading=True, num_workers=3)
 else:
     print("This script is designed to run only on Windows.")
