ff5801a77ff336eb9340ba752c8762e353309526
Xinhao <panxinhao98@gmail.com>
2025-04-03 14:59:50 +1100

    mini update

--- 0_runs_test.py
+++ 0_runs_test.py
@@ -1,6 +1,5 @@
 import luto.simulation as sim
 data = sim.load_data()
-import luto.simulation as sim
 sim.run(data=data, base_year=2010, target_year=2050)
 pkl_path = f'{data.path}/data_with_solution.gz'
 sim.save_data_to_disk(data,pkl_path)
