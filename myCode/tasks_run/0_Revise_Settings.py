3983b0a86ee92ac0b8146cc65ba3330906e85d22
Xinhao <panxinhao98@gmail.com>
2025-04-09 15:46:55 +1000

    fix bug for run tasks

--- myCode/tasks_run/0_Revise_Settings.py
+++ myCode/tasks_run/0_Revise_Settings.py
@@ -11,42 +11,49 @@ grid_search = {
         # 'snapshot',
         'timeseries'
     ],
-    'STEP_SIZE': [1],
+    'PENALTIES_WEIGHT': [0.05],
+    'BIODIV_WEIGHT': [0.5,1,10,50,100,500,1000,3000,5000,10000],
+    'STEP_SIZE': [5],
     'OBJECTIVE': ['maxprofit'], # maxprofit
     'WRITE_OUTPUT_GEOTIFFS': [False],
     'RESFACTOR': [20],
-    'GBF2_PRIORITY_DEGRADED_AREAS_PERCENTAGE_CUT': [50],
+
     # GHG settings
     'GHG_EMISSIONS_LIMITS' : ['on'],
-    'GHG_CONSTRAINT_TYPE': ['hard'],
+    'GHG_CONSTRAINT_TYPE': ['soft'],
     'GHG_LIMITS_FIELD': [
         # '1.5C (67%) excl. avoided emis',
-        # '1.5C (50%) excl. avoided emis',
-        '1.8C (67%) excl. avoided emis'
+        '1.5C (50%) excl. avoided emis',
+        # '1.8C (67%) excl. avoided emis'
     ],
-    'CARBON_PRICES_FIELD': ['c0'],
+    # 'CARBON_PRICES_FIELD': ['c0'],
+
     'BIODIVERSTIY_TARGET_GBF_2': ['on'],
     'GBF2_CONSTRAINT_TYPE': ['soft'],
     'BIODIV_GBF_TARGET_2_DICT': [
         # {2010: 0, 2030: 0, 2050: 0, 2100: 0 },
-        # {2010: 0, 2030: 0.3, 2050: 0.3, 2100: 0.3 },
-        {2010: 0, 2030: 0.3, 2050: 0.5, 2100: 0.5 }
+        {2010: 0, 2030: 0.3, 2050: 0.3, 2100: 0.3 },
+        # {2010: 0, 2030: 0.3, 2050: 0.5, 2100: 0.5 }
     ],
 
     # Water settings
     'WATER_LIMITS': ['on'],
     'WATER_CONSTRAINT_TYPE': ['soft'],
-    # Biodiversity settings
-    'DEMAND_CONSTRAINT_TYPE': ['soft'],
-    'BIODIVERSTIY_TARGET_GBF_3': ['off'],
-    'BIODIVERSTIY_TARGET_GBF_4': ['off'],
     'INCLUDE_WATER_LICENSE_COSTS ': [1],
-    'SOLVE_BIODIV_PRIORITY_WEIGHT': [10],
+    # Demand settings
+    'DEMAND_CONSTRAINT_TYPE': ['soft'],
+
+    # Biodiversity settings
+    'BIODIVERSTIY_TARGET_GBF_3': ['off'],
+    'BIODIVERSTIY_TARGET_GBF_4_SNES': ['off'],
+    'BIODIVERSTIY_TARGET_GBF_4_ECNES': ['off'],
+    'BIODIVERSTIY_TARGET_GBF_8': ['off'],
+    'GBF2_PRIORITY_DEGRADED_AREAS_PERCENTAGE_CUT': [50],
 }
 
-suffixs = ['GHG_LIMITS_FIELD', 'BIODIV_GBF_TARGET_2_DICT']
+suffixs = ['BIODIV_WEIGHT']
 
-output_file = os.path.join("Custom_runs", "setting_0408_test.csv")
-create_grid_search_template( grid_search,output_file,suffixs)
+output_file = os.path.join("Custom_runs", "setting_0409_test.csv")
+create_grid_search_template(grid_search,output_file,suffixs)
 print(f"saved to {output_file}")
 
