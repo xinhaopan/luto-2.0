dd52750b18adf876a4b691bb675b856c0bc74318
Xinhao <panxinhao98@gmail.com>
2025-04-10 13:09:49 +1000

    update transfer_html from nci

--- myCode/tasks_run/1_Run_tasks_linux_1.py
+++ myCode/tasks_run/1_Run_tasks_linux_1.py
@@ -2,9 +2,7 @@ from myCode.tasks_run.tools.helpers import create_task_runs
 import os
 
 if os.name == 'posix':
-    input_file = 'Custom_runs/setting_0408_cp.csv'
+    input_file = 'Custom_runs/setting_0409_test7.csv'
     create_task_runs(input_file, use_multithreading=False, num_workers=3)
 else:
     print("This script is designed to run only on Windows.")
-
-
