7f71c07f0949a86be6d09863ad540168dedbd3fb
Xinhao <panxinhao98@gmail.com>
2025-04-28 13:50:03 +1000

    update for watch paper1 data

--- myCode/draw_all/code/20_data_test.py
+++ myCode/draw_all/code/20_data_test.py
@@ -117,12 +117,133 @@ def add_value_label_column(df, unit):
 
     return df
 
+# import numpy as np
+# import sys
+# import os
+# import pandas as pd
+# from scipy.interpolate import interp1d
+# import matplotlib.pyplot as plt
+# from PIL izmport Image, ImageDraw, ImageFont
+#
+# from tools.data_helper import *
+# from tools.plot_helper import *
+# from tools.parameters import *
+#
+# plt.rcParams['font.family'] = 'Arial'
+#
+# # 获取到文件的绝对路径，并将其父目录添加到 sys.path
+# sys.path.append(os.path.abspath('../../../luto'))
+#
+# # 导入 settings.py
+# import settings
+#
+# font_size = 15
+# csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Water Net Yield (ML)', 'region'
+# area_dict = get_dict_data(input_files, csv_name, value_column_name, filter_column_name)
+# area_ag_group_dict,legend_colors = get_colors(area_dict, 'tools/land use colors.xlsx', sheet_name='water')
+# output_png = '../output/09_S3_water.png'
+# plot_Combination_figures(area_ag_group_dict, output_png, input_files, plot_stacked_bar, legend_colors,
+#                             n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=(0, 300),
+#                              x_ticks=10, y_ticks=100,
+#                              legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=4)
 
-
+import numpy as np
+import sys
+import os
 import pandas as pd
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+
+from tools.data_helper import *
+from tools.plot_helper import *
+from tools.parameters import *
+
+plt.rcParams['font.family'] = 'Arial'
+
+# 获取到文件的绝对路径，并将其父目录添加到 sys.path
+sys.path.append(os.path.abspath('../../../luto'))
+
+# 导入 settings.py
+import settings
+
+csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Value (ML)', 'Landuse Type'
+water_dict = get_dict_data(input_files, csv_name, value_column_name, filter_column_name,condition_column_name=['Climate Change existence'],
+                           condition_value=['Without CCI'])
+water_limit_dict = get_dict_data(input_files, 'water_yield_limits_and_public_land',
+                                 'Value (ML)', 'Type' ,condition_column_name=['CCI Existence'],
+                           condition_value=['HIST (ML)'])
+water_dict = {
+    k: pd.merge(
+        v,
+        water_limit_dict[k][['WNY Pubulic']].rename(columns={'WNY Pubulic': 'Public land'}),
+        left_index=True,
+        right_index=True,
+        how='left'
+    )
+    for k, v in water_dict.items()
+}
+water_dict,legend_colors = get_colors(water_dict, 'tools/land use colors.xlsx', sheet_name='lu')
+water_yeild_dict ={key: df.assign(**{'Yield': df.sum(axis=1)}) for key, df in water_dict.items()}
+water_point_dict = concatenate_dicts_by_year([water_limit_dict, water_yeild_dict])
+water_point_dict = rename_and_filter_columns(water_point_dict, ['WNY LIMIT','Yield'], ['Targets','Yield'])
+point_colors = ['red','black']
+
+output_png = '../output/09_S3_water'
+y_range, y_ticks = calculate_y_axis_range(water_dict,4)
+plot_Combination_figures(water_dict, output_png, input_files, plot_stacked_bar_and_line, legend_colors,point_dict=water_point_dict,point_colors=point_colors,
+                            n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=y_range,
+                             x_ticks=20, y_ticks=y_ticks,
+                             legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=2)
 
 
-def extract_land_use_stats(result_file, filter_str=None):
+csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Value (ML)',  'Landuse'
+water_ag_dict = get_dict_data(input_files, csv_name, value_column_name, filter_column_name,condition_column_name=['Climate Change existence'], condition_value=['Without CCI'])
+water_ag_group_dict = aggregate_by_mapping(water_ag_dict, 'tools/land use group.xlsx', 'desc', 'ag_group')
+water_ag_group_dict,legend_colors = get_colors(water_ag_group_dict, 'tools/land use colors.xlsx', sheet_name='ag_group')
+output_png = '../output/09_S3_water_ag_group'
+y_range, y_ticks = calculate_y_axis_range(water_ag_group_dict,4)
+plot_Combination_figures(water_ag_group_dict, output_png, input_files, plot_stacked_bar, legend_colors,
+                            n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=y_range,
+                             x_ticks=20, y_ticks=y_ticks,
+                             legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=2)
+
+csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Value (ML)',  'Landuse subtype'
+water_am_dict = get_dict_data(input_files, csv_name, value_column_name, filter_column_name,condition_column_name=['Climate Change existence'], condition_value=['Without CCI'])
+water_am_dict,legend_colors = get_colors(water_am_dict, 'tools/land use colors.xlsx', sheet_name='am')
+output_png = '../output/09_S3_water_am'
+y_range, y_ticks = calculate_y_axis_range(water_am_dict,4)
+plot_Combination_figures(water_am_dict, output_png, input_files, plot_stacked_bar, legend_colors,
+                            n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=y_range,
+                             x_ticks=20, y_ticks=y_ticks,
+                             legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=2)
+
+csv_name, value_column_name, filter_column_name = 'water_yield_separate', 'Value (ML)', 'Landuse'
+water_non_ag_dict = get_dict_data(input_files, csv_name, value_column_name, filter_column_name,condition_column_name=['Climate Change existence'], condition_value=['Without CCI'])
+water_non_ag_dict,legend_colors = get_colors(water_non_ag_dict, 'tools/land use colors.xlsx', sheet_name='non_ag')
+output_png = '../output/09_S3_water_non-ag'
+y_range, y_ticks = calculate_y_axis_range(water_non_ag_dict,3)
+plot_Combination_figures(water_non_ag_dict, output_png, input_files, plot_stacked_bar, legend_colors,
+                            n_rows=3, n_cols=3, font_size=font_size, x_range=(2010, 2050), y_range=y_range,
+                             x_ticks=20, y_ticks=y_ticks,
+                             legend_position=(0.5, -0.25), show_legend='last', legend_n_rows=2)
+
+
+
+group_dict = {key: df.assign(sum=df.sum(axis=1)) for key, df in water_dict.items()}
+substring = "*"
+target_column = '*' # next(iter(group_dict.values())).columns[0]
+year_filter = 2050
+
+filtered_dict = filter_dfs_by_substring(group_dict, substring)
+
+# 找出全局最大值和最小值
+global_result = find_global_min_max(filtered_dict, target_column, year_filter)
+result_result = add_value_label_column(global_result, "Mha")
+result_result
+
+
+def extract_land_use_stats(result_file, filter_str=None, year=2050):
     """
     从Excel文件中提取每种土地利用类型在2050年的最大值和最小值，并找到对应的工作表名称。
     额外计算一级列名是2050，二级列名是'Area×Distance'的最大值和最小值。
@@ -149,7 +270,7 @@ def extract_land_use_stats(result_file, filter_str=None):
         # 确保列索引是MultiIndex
         if isinstance(df.columns, pd.MultiIndex):
             # 过滤出2050年的数据
-            year_2050_cols = [col for col in df.columns if col[0] == 2050]
+            year_2050_cols = [col for col in df.columns if col[0] == year]
             if year_2050_cols:
                 df_2050 = df.loc[:, year_2050_cols]
                 # 获取土地利用类型
@@ -168,7 +289,7 @@ def extract_land_use_stats(result_file, filter_str=None):
                     min_value = numeric_cols.min().min()
 
                     # 计算 'Area×Distance' 的最大值和最小值
-                    area_distance_col = (2050, 'Area×Distance')
+                    area_distance_col = (year, 'Area×Distance')
                     if area_distance_col in df_land_use.columns:
                         max_area_distance = df_land_use[area_distance_col].max()
                         min_area_distance = df_land_use[area_distance_col].min()
@@ -193,4 +314,10 @@ def extract_land_use_stats(result_file, filter_str=None):
 
 result_file = "../output/12_land_use_movement_all.xlsx"
 filter_str = None
-results_df = extract_land_use_stats(result_file, filter_str)
+results_df = extract_land_use_stats(result_file, filter_str,2050)
+mask = (
+    (results_df['Land Use'] == 'Winter cereals')
+    # & (results_df['Max Sheet Name'].str.contains('BIO_High'))
+)
+filtered = results_df[mask]
+filtered
