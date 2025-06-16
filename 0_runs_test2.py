2603a0a888ba8e191c551fe18f8f99e45638d695
Xinhao <panxinhao98@gmail.com>
2025-06-16 17:24:38 +1000

    merge: jinzhu 20250616

---
+++ 0_runs_test2.py
@@ -0,0 +1,10 @@
+import os
+# os.environ['GRB_LICENSE_FILE'] = r'C:\Users\s222552331\gurobi\gurobi_xp.lic'
+import luto.simulation as sim
+
+data = sim.load_data()
+sim.run(data=data)
+pkl_path = f'{data.path}/data_with_solution.gz'
+sim.save_data_to_disk(data,pkl_path)
+from luto.tools.write import write_outputs
+write_outputs(data)
