46824fd406749cba4850179783e5b36de6fcf7d5
Xinhao <panxinhao98@gmail.com>
2025-07-22 16:12:01 +1000

    update: run for no ghg and bio

--- myCode/tasks_run/Paper2_tasks.py
+++ myCode/tasks_run/Paper2_tasks.py
@@ -20,7 +20,7 @@ grid_search = {
     'SOLVE_WEIGHT_BETA': [0.9],
     'OBJECTIVE': ['maxprofit'], # maxprofit
     'WRITE_OUTPUT_GEOTIFFS': [True],
-    'RESFACTOR': [1],
+    'RESFACTOR': [3],
     'SIM_YEARS': [[i for i in range(2010,2051,1)]],
 
     # ----------------------------------- GHG settings --------------------------------
