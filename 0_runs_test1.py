e374260b61e941482b21b73b48fc57e1cc89edb8
Xinhao <panxinhao98@gmail.com>
2025-08-23 13:40:05 +1000

    update: create env

--- 0_runs_test1.py
+++ 0_runs_test1.py
@@ -27,8 +27,6 @@ def monitor_memory(func, *args, **kwargs):
 def main():
 
     try:
-        os.makedirs('output', exist_ok=True)
-
         # 记录模拟开始的时间
         write_log("Simulation started")
         overall_start_time = time.time()
@@ -38,22 +36,8 @@ def main():
         write_log(f"Data loaded. Peak memory usage: {load_data_memory:.2f} GB")
 
         # 监控运行模拟
-        _, simulation_memory = monitor_memory(sim.run, data=data)
-        write_log(f"Simulation completed. Peak memory usage: {simulation_memory:.2f} GB")
-
-        # 保存数据并监控内存
-        years = [i for i in settings.SIM_YEARS if i <= data.last_year]
-        data.set_path(years)
-        pkl_path = f'{data.path}/data_with_solution.gz'
-        write_log("Start saving data with solution...")
-        _, save_data_memory = monitor_memory(sim.save_data_to_disk, data, pkl_path)
-        write_log(f"Data with solution saved in {data.path}. Peak memory usage: {save_data_memory:.2f} GB")
-
-        # 监控write_output
-        write_log("Start writing output...")
-        from luto.tools.write import write_outputs
-        _, write_output_memory = monitor_memory(write_outputs, data)
-        write_log(f"Output written. Peak memory usage: {write_output_memory:.2f} GB")
+        _, run_memory = monitor_memory(sim.run, data=data)
+        write_log(f"Simulation completed. Peak memory usage: {run_memory:.2f} GB")
 
         # 总结束时间
         overall_end_time = time.time()
@@ -72,7 +56,7 @@ def main():
 
         # 记录日志
         write_log(f"Total run time: {formatted_duration}")
-        write_log(f"Overall peak memory usage: {max(load_data_memory, simulation_memory,save_data_memory, write_output_memory):.2f} GB")
+        write_log(f"Overall peak memory usage: {max(load_data_memory, run_memory):.2f} GB")
 
 
     except Exception as e:
