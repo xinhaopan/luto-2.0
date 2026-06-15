fix(data): drop LUMASK from GBF4 SNES/ECNES resfactored averages
LUMASK was zeroing species presence outside current ag cells, but
GBF4 SNES/ECNES areas should reflect raw pre-1750 presence regardless
of current land use mask.

luto/data.py
@@ -1534,7 +1534,7 @@ class Data:
                     self.get_resfactored_average_fraction(
                         self.GBF4_SNES_LAYERS_ALL.sel(
                             species=sp, presence=('LIKELY' if pres == 'LIKELY' else 'LIKELY_AND_MAYBE')
-                        ).data.todense().astype(np.float32) * self.LUMASK,
+                        ).data.todense().astype(np.float32),
                         use_valid_cell_count=False,
                     )
                     for sp, pres in sel_sp_pres_pairs
@@ -1562,7 +1562,7 @@ class Data:
                     self.get_resfactored_average_fraction(
                         self.GBF4_ECNES_LAYERS_ALL.sel(
                             species=comm, presence=('LIKELY' if pres == 'LIKELY' else 'LIKELY_AND_MAYBE')
-                        ).data.todense().astype(np.float32) * self.LUMASK,
+                        ).data.todense().astype(np.float32),
                         use_valid_cell_count=False,
                     )
                     for comm, pres in sel_comm_pres_pairs